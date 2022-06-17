e17bf164956e7f5279f187f83a5503199cbd332d
hiroRti <wwt12138@gmail.com>
2022-06-17 21:24:06 +0800

    test

--- engines/dmhyorg.py
+++ engines/dmhyorg.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#VERSION: 1.0
+#VERSION: 1.1
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
